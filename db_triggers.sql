de56d92e5e619d69627166a1af613d88807ad23a
mikhachev <36811961+mikhachev@users.noreply.github.com>
2019-08-13 15:13:34 +0300

    Update db_triggers.sql

--- db_triggers.sql
+++ db_triggers.sql
@@ -1,1 +1,6 @@
+#  Workbench тоже вылетает на триггерах. Возможно будет такое рашение:
 
+CREATE DEFINER = CURRENT_USER TRIGGER `employees`.`salaries_AFTER_INSERT` AFTER INSERT ON `salaries` FOR EACH ROW
+BEGIN
+insert into salaries set salaries.emp_no = new emp_no, salaries.salary=10000
+END
