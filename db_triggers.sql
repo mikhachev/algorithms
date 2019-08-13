6ac7626e0096fcb81d137fcbca9db15288d8241c
mikhachev <36811961+mikhachev@users.noreply.github.com>
2019-08-13 15:33:05 +0300

    Update db_triggers.sql

--- db_triggers.sql
+++ db_triggers.sql
@@ -1,16 +1,14 @@
-#  Workbench тоже вылетает на триггерах. Возможно будет такое решение:
-
 CREATE DEFINER = CURRENT_USER TRIGGER `employees`.`salaries_AFTER_INSERT` AFTER INSERT ON `salaries` FOR EACH ROW
 BEGIN
-insert into salaries set as sl
-('sl'.`emp_no`,
-'sl'.`salary`,
-'sl'.`from_date`,
-'sl'.`to_date`
+insert into salaries  
+(`emp_no`,
+`salary`,
+`from_date`,
+`to_date`
 )
 VALUES
 (new.emp_no,
 10000,
-current timestamp,
-current timestamp + 24*3600)
+now(),
+now() + 24*3600);
 END
